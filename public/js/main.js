01384bf9ba5ef5e89109ab3385a08b9456254a6b
Abhijeet Bhatikar <abhijeet4jobs@gmail.com>
2021-09-14 14:43:26 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -22,17 +22,39 @@ if(location.href.substr(0,5) !== 'https')
  * RTCPeerConnection configuration 
  */
 const configuration = {
-    "iceServers": [{
-            "urls": "stun:stun.l.google.com:19302"
-        },
-        // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
-        // set your own servers here
-        {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        }
-    ]
+//    "iceServers": [{
+//            "urls": "stun:stun.l.google.com:19302"
+//        },
+//        // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
+//        // set your own servers here
+//        {
+//            url: 'turn:192.158.29.39:3478?transport=udp',
+//            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+//            username: '28224511:1379330808'
+//        }
+//    ]
+    
+    "iceServers": [
+                    {   "urls": 'stun:stun.l.google.com:19302'},
+                    {   "urls": 'stun:global.stun.twilio.com:3478?transport=udp'},
+                    {   "urls": 'stun:66.102.1.127:19302'},
+                    {   "urls": 'stun:[2A00:1450:400C:C06::7F]:19302'},
+                    {
+                        url: 'turn:numb.viagenie.ca',
+                        credential: 'muazkh',
+                        username: 'webrtc@live.com'
+                    },
+                    {
+                        url: 'turn:192.158.29.39:3478?transport=udp',
+                        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                        username: '28224511:1379330808'
+                    },
+                    {
+                        url: 'turn:192.158.29.39:3478?transport=tcp',
+                        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                        username: '28224511:1379330808'
+                    }
+                ]
 }
 
 /**
